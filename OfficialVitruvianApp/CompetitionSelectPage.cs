cd115069c0f76f374de4fbeb3ad9f1e9e627e9ff
Fazlul Zubair <unknown-user@fe80::bd58:3fb2:edff:5b9%10>
2014-11-09 14:43:43 -0800

    competition select table now working

--- OfficialVitruvianApp/CompetitionSelectPage.cs
+++ OfficialVitruvianApp/CompetitionSelectPage.cs
@@ -21,7 +21,7 @@ namespace OfficialVitruvianApp
 			oneBtn.TextColor = Color.Green;
 			oneBtn.BackgroundColor = Color.Black;
 			oneBtn.Clicked += (object sender, EventArgs e) => {
-				//This button should change the table of teams to show only teams attending this competition.
+				//This button should change the table of matches to show only matches for this competition.
 			};
 
 
@@ -31,7 +31,7 @@ namespace OfficialVitruvianApp
 			twoBtn.TextColor = Color.Green;
 			twoBtn.BackgroundColor = Color.Black;
 			twoBtn.Clicked += (object sender, EventArgs e) => {
-				//This button should change the table of teams to show only teams attending this competition.
+				//This button should change the table of matches to show only matches for this competition.
 			};
 
 
@@ -41,12 +41,48 @@ namespace OfficialVitruvianApp
 			threeBtn.TextColor = Color.Green;
 			threeBtn.BackgroundColor = Color.Black;
 			threeBtn.Clicked += (object sender, EventArgs e) => {
-				//This button should change the table of teams to show only teams attending this competition.
+				//This button should change the table of matches to show only matches for this competition.
 			};
 
 
-			//A table with a bunch of other teams that changes based on the selected competition.
-			//Code here
+			//A table with a bunch of other matches that changes based on the selected competition.
+			StackLayout matchStack = new StackLayout ();
+
+			ScrollView matchList = new ScrollView ();
+			matchList.Content = matchStack;
+			matchList.HeightRequest = 150;
+			MatchListCell cell1 = new MatchListCell ();
+			cell1.matchName.Text = "1";
+			MatchListCell cell2 = new MatchListCell ();
+			cell2.matchName.Text = "2";
+			MatchListCell cell3 = new MatchListCell ();
+			cell3.matchName.Text = "3";
+			MatchListCell cell4 = new MatchListCell ();
+			cell4.matchName.Text = "4";
+			MatchListCell cell5 = new MatchListCell ();
+			cell5.matchName.Text = "5";
+			matchStack.Children.Add (cell1);
+			matchStack.Children.Add (cell2);
+			matchStack.Children.Add (cell3);
+			matchStack.Children.Add (cell4);
+			matchStack.Children.Add (cell5);
+
+			/*Grid teamGrid = new Grid {
+				HorizontalOptions = LayoutOptions.FillAndExpand,
+				RowDefinitions = {
+					new RowDefinition {Hieght = GridLength.Auto},
+					new RowDefinition {Hieght = GridLength.Auto},
+					new RowDefinition {
+					}
+				},
+				ColumnDefinitions = {
+				},
+
+				Children = {
+
+				}
+
+			};*/
 
 			//Back Button Navigation. Could be in the corner or on the bottom.
 			Button backBtn = new Button ();
@@ -56,6 +92,21 @@ namespace OfficialVitruvianApp
 
 			backBtn.Clicked += (object sender, EventArgs e) => {
 				//return new NavigationPage (new MainMenuPage ());
+			};
+
+			//Page Layout
+			this.Content = new StackLayout {
+				HorizontalOptions = LayoutOptions.CenterAndExpand,
+				VerticalOptions = LayoutOptions.CenterAndExpand,
+
+				Children = {
+					competitionLabel,
+					oneBtn,
+					twoBtn,
+					threeBtn,
+					matchList,
+					backBtn
+				}
 			};
 		}
 	}
---
+++ OfficialVitruvianApp/Views/MatchListCell.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace OfficialVitruvianApp
+{
+	public class MatchListCell:ContentView
+	{
+		public Label matchName;
+
+		public MatchListCell ()
+		{
+			matchName = new Label ();
+			WidthRequest = 100;
+			HeightRequest = 50;
+
+			Content = matchName;
+		}
+	}
+}
+
